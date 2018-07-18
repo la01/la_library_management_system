1676ab0df20e58afd57602f507b56ff7b72ad27e
kajisa <dobobobobe@gmail.com>
2018-07-18 13:09:49 +0900

    ログインID、パスワードを外だし

--- src/la/servlet/LoginServlet.java
+++ src/la/servlet/LoginServlet.java
@@ -1,6 +1,8 @@
 package la.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,17 +19,29 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("id");
-		String passwd = request.getParameter("passwd");
-
-		if("id".equals(id) && "password".equals(passwd)) {
-			request.getSession().setAttribute("login", true);
-			request.getRequestDispatcher("WEB-INF/jsp/index.jsp").forward(request, response);
-		} else {
+		try {
+			String inputId = request.getParameter("id");
+			String inputPasswd = request.getParameter("passwd");
+			
+			InputStream in = getClass().getResourceAsStream("/login.properties");
+			Properties prop = new Properties();
+			prop.load(in);
+			String id = prop.getProperty("ID");
+			String passwd = prop.getProperty("PASSWORD");
+			
+			if(id.equals(inputId) && passwd.equals(inputPasswd)) {
+				request.getSession().setAttribute("login", true);
+				request.getRequestDispatcher("WEB-INF/jsp/index.jsp").forward(request, response);
+			} else {
+				request.setAttribute("title", "ログインエラー");
+				request.setAttribute("body", "IDかパスワードが間違っています");
+				request.getRequestDispatcher("Error").forward(request, response);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
 			request.setAttribute("title", "ログインエラー");
 			request.setAttribute("body", "IDかパスワードが間違っています");
 			request.getRequestDispatcher("Error").forward(request, response);
 		}
 	}
-
 }
